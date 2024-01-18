7b497155d892554d1906357435609b54af4fb4ab
safrilth <safriltaufiqhidayat@gmail.com>
2024-01-18 19:09:10 +0700

    final commit

---
+++ guide/style-guide.md
@@ -0,0 +1,53 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+--color-primary-1: hsl(0, 100%, 67%);
+--color-primary-2: hsl(39, 100%, 56%);
+--color-primary-3: hsl(166, 100%, 37%);
+--color-primary-4: hsl(234, 85%, 45%);
+
+## Gradients
+
+--color-gradient-1: hsl(252, 100%, 67%);
+--color-gradient-2: hsl(241, 81%, 54%);
+--color-gradient-3: hsla(256, 72%, 46%, 1);
+--color-gradient-4: hsla(241, 72%, 46%, 0);
+
+
+
+### Neutral
+
+--color-neutral-1: hsl(0, 0%, 100%);
+--color-neutral-2: hsl(221, 100%, 96%);
+--color-neutral-3: hsl(241, 100%, 89%);
+--color-neutral-4: hsl(224, 30%, 27%);
+
+### Notes
+
+Use transparency to get the colour variations necessary to match the design. Hint: look into using `hsla()`.
+
+## Typography
+
+### Body Copy
+
+--fs-body:18px;
+
+### Font
+
+- Family: [Hanken Grotesk](https://fonts.google.com/specimen/Hanken+Grotesk)
+- Weights: 500, 700, 800
+
+--ff-body: "Hanken Grotesk";
+--fw-light: 500;
+--fw-medium: 700;
+--fw-bold: 800;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,156 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import memory from "$lib/assets/images/icon-memory.svg"
+  import reaction from "$lib/assets/images/icon-reaction.svg"
+  import verbal from "$lib/assets/images/icon-verbal.svg"
+  import visual from "$lib/assets/images/icon-visual.svg"
+</script>
+
+<article>
+  <section class="summary">
+    <p class="text-grey bold">Your Result</p>
+    <div class="score">
+      <h2 class="text-white" style="font-size: xx-large;">76</h2>
+      <p class="text-grey">of 100</p>
+    </div>
+    <h3 class="text-white">Great</h3>
+    <p class="text-grey " >You scored higher than 65% of the people who have taken these tests.</p>
+  </section>
+  <section class="details">
+    <h4>Summary</h4>
+    <div class="list red bold">
+      <img src={reaction} alt="reaction icon" >
+      <p class=" text-red">Reaction</p>
+      <p>80<span class="text-grey">/100</span></p>
+    </div>
+    <div class="list yellow bold">
+      <img src={memory} alt="memory icon" >
+      <p class=" text-yellow">Memory</p>
+      <p>92<span class="text-grey">/100</span></p>
+    </div>
+    <div class="list green bold">
+      <img src={verbal} alt="verbal icon" >
+      <p class=" text-green">Verbal</p>
+      <p>61<span class="text-grey">/100</span></p>
+    </div>
+    <div class="list blue bold">
+      <img src={visual} alt="visual icon" >
+      <p class=" text-blue">Visual</p>
+      <p>72<span class="text-grey">/100</span></p>
+    </div>
+    <button>Continue</button>
+  </section>
+</article>
+
+<style>
+  article {
+    margin-inline: 2rem;
+    /* border: 1px solid red; */
+    border-radius: 2rem;
+    box-shadow: 2px 4px 6px 8px var(--color-neutral-2);
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+  }
+
+  @media(width >= 640px){
+    article {
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr;
+    }
+  }
+
+  .summary {
+    background-image: radial-gradient(80% 80% at center , var(--color-gradient-1) 20% , var(--color-gradient-2)) ;
+    border-radius: 2rem;
+    display: grid;
+    place-content: center;
+    text-align: center;
+    gap: 1rem;
+    padding: 1rem;
+    max-width: 20rem;
+  }
+
+  .score {
+    background-image: linear-gradient(var(--color-gradient-3), var(--color-gradient-2));
+    margin-inline: auto;
+    padding: 2rem;
+    border-radius: 50%;
+    aspect-ratio: 1;
+  }
+
+  .details{
+    background-color: var(--color-neutral-1);
+    border-radius: 0 0 2rem 2rem;
+    overflow: hidden;
+    display: grid;
+    padding-block: 1rem;
+    padding-inline: 1.2rem;
+    /* border: 1px solid red; */
+    gap: 0.8rem;
+  }
+
+  .list {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    border-radius: 0.5rem;
+    padding: 0.8rem;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  /* color */
+  .red {
+    background-color: var(--color-primary-1);
+  }
+  
+  .text-red{
+    color: var(--fc-primary-1);
+  }
+
+  .yellow {
+    background-color: var(--color-primary-2);
+  }
+  
+  .text-yellow{
+    color: var(--fc-primary-2);
+  }
+
+  .green {
+    background-color: var(--color-primary-3);
+  }
+  
+  .text-green{
+    color: var(--fc-primary-3);
+  }
+
+  .blue{
+    background-color: var(--color-primary-4);
+  }
+  
+  .text-blue{
+    color: var(--fc-primary-4);
+  }
+
+  .text-grey{
+    color: var(--color-neutral-3);
+  }
+
+  .text-white{
+    color: var(--color-neutral-1)
+  }
+
+
+  .bold {
+    font-weight: var(--fw-bold);
+    letter-spacing: 1.5px;
+  }
+
+  button {
+    padding: 0.8rem;
+    border-radius: 2rem;
+    /* background-color: transparent; */
+    border: none;
+    background-color: var(--color-neutral-4);
+    color: var(--color-neutral-1);
+    margin-block: 0.5rem;
+  }
+</style>
